c26f4a597e9c935f16602bdd9742faa97f95ed56
Frank Henkourth <145686933+FrankHenkourth@users.noreply.github.com>
2024-07-18 03:22:27 -0400

    Create Functions and methods.cpp
    v0.1.0

---
+++ Functions and methods.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+//Subprogramas o Funciones
+
+//Subprograma basico
+int suma(a,b){              //Declaras el tipo de dato que va a retornar | El nombre de el subprograma | Los paramatros enviados para ejecutar.
+	resultado = a + b;      //Logica de el subprograma.
+	return resultado;       //Retorna el dato que se quiera visualizar cuando se ejecute la llamada.      
+}
+
+bool esPar(a){
+	resto=a%2;
+	if(resto==0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+int fibonacci(pos){
+
+}
